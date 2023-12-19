bb277f1d42fe082ef3b536c62d6d97eb17533045
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2023-12-19 22:22:57 +0100

    First update

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,70 @@
 import { Container, Box, Typography, Button, SvgIcon } from "@mui/material";
 import Image from 'next/image';
 
-import Hero from "./components/Hero";
-
 export default function Home() {
   return (
     <Box>
-      <Hero />
+      {/* HERO */}
+      <Box
+        sx={{
+            position: 'relative',
+            width: '100%',
+            height: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+        }}
+    >
+        <Image
+            src="/images/nexus-hero-2.png"
+            alt="Nexus hero image"
+            fill={true}
+            objectFit="cover"
+            style={{
+              zIndex: -1,
+            }}
+        />
+        <Typography variant="subtitle1" color="primary.contrastText" sx={{ mb: 3 }}>
+            ...initializing nexus_v1.0.0
+        </Typography>
+        <Typography variant="h1" component="h1" color="primary.contrastText">
+            Enter Nexus
+        </Typography>
+        <Typography variant="h5" color="primary.contrastText" sx={{ my: 3 }}>
+            Join our waitlist and stay updated
+        </Typography>
+        <Button
+            variant="outlined"
+            size="large"
+            color="primary"
+        >
+            Send
+        </Button>
+    </Box>
+
+        {/* MAIN CONTENT */}
       <Container>
-        
+        {/* USP */}
+        <Box>
+          
+              <Box className="flex flex-col justify-center p-12 my-24 mx-12 space-y-6 rounded-xl bg-gray-800 border border-gray-700 shadow-2xl shadow-black">
+              
+              <Typography variant="h3" component="h3" color="primary.contrastText">USP</Typography>
+              <Typography variant="subtitle1" component="" color="primary.contrastText">USP</Typography>
+            </Box>
+        </Box>
+            
+        {/* VISION */}
+
+        {/* ROADMAP */}
+
+        {/* PLAYTEST */}
+
+        {/* ABOUT */}
+
+        {/* CTA */}
       </Container>
     </Box>
     
